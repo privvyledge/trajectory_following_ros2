Fixed feasibility issues with Casadi QP solvers and Acados caused by indefinite Hessians and rate constraints. Fixed by adding a slack variable to the inpu rate of change, using exact hessian approximation and adding convexification hessian regularization.

trajectory_following_ros2/acados/acados_settings.py
@@ -70,7 +70,8 @@ def acados_settings(Tf, N, x0=None, scale_cost=True,
     ns = nsh + nsbx  # total number of slacks at stages (1, N-1)
 
     # discretization
-    ocp.dims.N = N  # prediction horizon
+    # ocp.dims.N = N  # prediction horizon (depracated)
+    ocp.solver_options.N_horizon = N  # prediction horizon
 
     # set cost
     '''
@@ -240,7 +241,7 @@ def acados_settings(Tf, N, x0=None, scale_cost=True,
     ocp.constraints.ubu = np.array([model.acc_max, model.delta_max])  # input upper bound
     ocp.constraints.idxbu = np.array([0, 1])
 
-    # # Soft/slack state bounds: to map lower and upper slack vectors onto x(T).
+    # # Soft/slack state bounds: to map lower and upper slack vectors onto x(T). todo: enable
     # ocp.constraints.lsbx = np.array([-2 * np.pi])  # np.zeros([nsbx])
     # ocp.constraints.usbx = np.array([2 * np.pi])  # np.zeros([nsbx])
     # ocp.constraints.idxsbx = np.array(range(nsbx))
@@ -277,27 +278,29 @@ def acados_settings(Tf, N, x0=None, scale_cost=True,
         ocp.constraints.x0 = x0
 
     # set QP solver and integration
-    ocp.solver_options.qp_solver = "FULL_CONDENSING_HPIPM"
+    ocp.solver_options.qp_solver = "PARTIAL_CONDENSING_HPIPM"
     # PARTIAL_CONDENSING_HPIPM, FULL_CONDENSING_QPOASES, FULL_CONDENSING_HPIPM,
     # PARTIAL_CONDENSING_QPDUNES, PARTIAL_CONDENSING_OSQP, FULL_CONDENSING_DAQP
-    ocp.solver_options.nlp_solver_type = "SQP"  # SQP_RTI, SQP. SQP_RTI does only one iteration while SQP solves to a certain tolerance
-    ocp.solver_options.hessian_approx = "GAUSS_NEWTON"  # 'GAUSS_NEWTON', 'EXACT'
+    ocp.solver_options.nlp_solver_type = "SQP_RTI"  # SQP_RTI, SQP. SQP_RTI does only one iteration while SQP solves to a certain tolerance
+    # ocp.solver_options.globalization = 'MERIT_BACKTRACKING'  # turns on globalization. 'FUNNEL_L1PEN_LINESEARCH' if not self.use_RTI else 'MERIT_BACKTRACKING'
+    ocp.solver_options.hessian_approx = "EXACT"  # 'GAUSS_NEWTON', 'EXACT'.
     ocp.solver_options.integrator_type = "ERK"  # 'IRK' (implicit), 'ERK' (explicit), 'GNSF', 'DISCRETE', 'LIFTED_IRK'
     # ocp.solver_options.collocation_type = 'EXPLICIT_RUNGE_KUTTA'  # 'GAUSS_RADAU_IIA', 'GAUSS_LEGENDRE', 'EXPLICIT_RUNGE_KUTTA'
-    # ocp.solver_options.hpipm_mode = 'SPEED'  # 'BALANCE', 'SPEED_ABS', 'SPEED', 'ROBUST'
+    ocp.solver_options.hpipm_mode = 'ROBUST'  # 'BALANCE', 'SPEED_ABS', 'SPEED', 'ROBUST'
     # NO_REGULARIZE, MIRROR, PROJECT, CONVEXIFY, PROJECT_REDUC_HESS
-    # ocp.solver_options.regularize_method = "NO_REGULARIZE"
+    ocp.solver_options.regularize_method = "PROJECT"
+    ocp.solver_options.reg_epsilon = 1e-4
     ocp.solver_options.print_level = 0
     ocp.solver_options.sim_method_num_stages = 4  # (1) RK1, (2) RK2, (4) RK4
     ocp.solver_options.sim_method_num_steps = 3
     # ocp.solver_options.nlp_solver_step_length = 0.05
     ocp.solver_options.nlp_solver_max_iter = num_iterations
-    ocp.solver_options.tol = tolerance  # 1e-4
+    # ocp.solver_options.tol = tolerance  # 1e-4
     # ocp.solver_options.nlp_solver_tol_comp = 1e-1
-    ocp.solver_options.qp_solver_cond_N = 1  # int(N / 2)  # or N if scale_cost=False
-    ocp.solver_options.qp_solver_warm_start = 2
+    # ocp.solver_options.qp_solver_cond_N = 1  # int(N / 2)  # or N if scale_cost=False
+    # ocp.solver_options.qp_solver_warm_start = 2
     ocp.solver_options.qp_solver_iter_max = num_iterations
-    ocp.solver_options.qp_tol = tolerance  # 1e-3
+    # ocp.solver_options.qp_tol = tolerance  # 1e-3
 
     # set prediction horizon
     ocp.solver_options.tf = Tf

trajectory_following_ros2/casadi/kinematic_bicycle_model.py
@@ -1,3 +1,9 @@
+"""
+Todo:
+    * cleanup and make a simple extensible class/interface
+    * move helper functions to a different file
+    * add frenet model (https://github.com/bohlinz/genesis_path_follower/blob/master/scripts/controllers/kinematic_frenet_mpc.py | https://github.com/acados/acados/blob/master/examples/acados_python/race_cars/bicycle_model.py#L97)
+"""
 import types
 
 import numpy as np
@@ -11,12 +17,14 @@ class KinematicBicycleModel(object):
                  vel_bound=(-5.0, 5.0), delta_bound=(-23.0, 23.0), acc_bound=(-3.0, 3.0),
                  jerk_bound=(-1.5, 1.5), delta_rate_bound=(-352.9411764706, 352.9411764706),
                  vehicle_parameters=None, sample_time=0.001, symbol_type='MX',
-                 model_type='kinematic', model_name='vehicle_kinematic_model', discretization_method='cvodes'):
+                 model_type='kinematic', model_name='vehicle_kinematic_model', discretization_method='cvodes',
+                 discrete=False):
         """Constructor for KinematicBicycleModel"""
         # model info
         self.model_name = model_name
         self.model_type = model_type
         self.symbol_type = symbol_type
+        self.discrete = discrete
 
         # vehicle parameters
         if vehicle_parameters is None:
@@ -33,23 +41,27 @@ class KinematicBicycleModel(object):
         self.discretization_method = discretization_method
 
         # constraints
-        self.vel_bound = (-5.0, 5.0)
-        self.delta_bound = (-23.0, 23.0)
-        self.acc_bound = (-3.0, 3.0)
-        self.jerk_bound = (-1.5, 1.5)
-        self.delta_rate_bound = (-352.9411764706, 352.9411764706)
+        self.vel_bound = vel_bound
+        self.delta_bound = delta_bound
+        self.acc_bound = acc_bound
+        self.jerk_bound = jerk_bound
+        self.delta_rate_bound = acc_bound
 
-        self.model, self.constraints = self.setup_model(symbol_type=self.symbol_type)
-        [self.linear_model, self.ode_linear_function,
-         self.system_matrices, self.system_matrices_function_handles] = self.setup_linearizer(self.model.f_expl_expr,
-                                                                                              y_meas=None,
-                                                                                              x=self.model.x,
-                                                                                              u=self.model.u,
-                                                                                              xss=self.model.xss,
-                                                                                              uss=self.model.u0,
-                                                                                              wheelbase=self.model.params.wheelbase,
-                                                                                              method='jac')
-        self.linear_model, x_next = self.linearize(self.linear_model, wheelbase=self.model.params.wheelbase)
+        if self.discrete:
+            self.model, self.constraints = self.setup_discrete_ltv_model(symbol_type=self.symbol_type)
+            self.linear_model = self.model
+        else:
+            self.model, self.constraints = self.setup_model(symbol_type=self.symbol_type)
+            [self.linear_model, self.ode_linear_function,
+             self.system_matrices, self.system_matrices_function_handles] = self.setup_linearizer(self.model.f_expl_expr,
+                                                                                                  y_meas=None,
+                                                                                                  x=self.model.x,
+                                                                                                  u=self.model.u,
+                                                                                                  xss=self.model.xss,
+                                                                                                  uss=self.model.u0,
+                                                                                                  wheelbase=self.model.params.wheelbase,
+                                                                                                  method='jac')
+            self.linear_model, x_next = self.linearize(self.linear_model, wheelbase=self.model.params.wheelbase)
 
     def setup_model(self, symbol_type='MX',
                     z0=None, u0=(0., 0.),
@@ -221,6 +233,175 @@ class KinematicBicycleModel(object):
         # model.f_disk = self.setup_discretizer(z, u, parameters)
         model.x = z
         model.xss = zss
+        model.xdot = z_dot
+        model.u = u
+        model.u0 = u_prev
+        model.z = z_algebraic
+        model.p = parameters
+        model.name = self.model_name
+        model.params = params
+
+        return model, constraint
+
+    def setup_discrete_ltv_model(self, symbol_type='MX',
+                    z0=None, u0=(0., 0.),
+                    vel_bound=None, delta_bound=None, acc_bound=None, jerk_bound=None, delta_rate_bound=None):
+        # todo: remove constraints from model since that isn't from the kinematics
+        # todo: create new models class
+        # initialize structs
+        constraint = types.SimpleNamespace()  # todo: switch to a class instead
+        model = types.SimpleNamespace()  # todo: switch to a class instead
+
+        if symbol_type.lower() == 'MX'.lower():
+            symbol_type = casadi.MX
+
+        elif symbol_type.lower() == 'SX'.lower():
+            symbol_type = casadi.SX
+
+        else:
+            raise Exception('Invalid Symbol Type')
+
+        # state
+        x = symbol_type.sym("x")
+        y = symbol_type.sym("y")
+        vel = symbol_type.sym("vel")
+        psi = symbol_type.sym("psi")
+        z = casadi.vertcat(x, y, vel, psi)
+        num_states = z.shape[0]
+
+        # input
+        acc = symbol_type.sym("acc")
+        delta = symbol_type.sym("delta")
+        u = casadi.vertcat(acc, delta)
+        num_inputs = u.shape[0]
+
+        # initial input
+        acc_prev = symbol_type.sym("acc_prev")
+        delta_prev = symbol_type.sym("delta_prev")
+        u_prev = casadi.vertcat(acc_prev, delta_prev)
+        if u0 is None:
+            u0 = u_prev
+
+        # input rate symbols.
+        jerk = symbol_type.sym("jerk")
+        jerk = acc - acc_prev
+
+        delta_rate = symbol_type.sym("delta_rate")
+        delta_rate = delta - delta_prev
+
+        u_dot1 = casadi.vertcat(jerk, delta_rate)
+        u_dot2 = u - u0  #
+        u_dot = u - u_prev
+
+        # state derivatives
+        x_dot = symbol_type.sym("xdot")  # vel * casadi.cos(psi)
+        y_dot = symbol_type.sym("ydot")  # vel * casadi.sin(psi)
+        vel_dot = symbol_type.sym("vel_dot")  # acc
+        psi_dot = symbol_type.sym("psi_dot")  # (vel / wheelbase) * casadi.tan(delta)
+        z_dot = casadi.vertcat(x_dot, y_dot, vel_dot, psi_dot)  # ode-nonlinear
+
+        # algebraic variables
+        z_algebraic = casadi.vertcat([])
+
+        # parameters
+        '''Race car parameters'''
+        wheelbase = symbol_type.sym('wheelbase')  # m
+        dt = symbol_type.sym('dt')
+        z_ref = symbol_type.sym('z_ref', z.shape)
+        z_k = symbol_type.sym('z_k', z.shape)
+        u_ref = symbol_type.sym('u_ref', u.shape)
+        u_prev = symbol_type.sym('u_prev', u.shape)
+        parameters = casadi.vertcat(
+            wheelbase,
+            z_ref,
+            u_ref,
+            z_k,
+            u_prev,
+            dt
+    )
+
+        A = casadi.blockcat([
+            [1, 0, casadi.cos(psi) * dt, -vel * casadi.sin(psi) * dt],
+            [0, 1, casadi.sin(psi) * dt, vel * casadi.cos(psi) * dt],
+            [0, 0, 1, 0],
+            [0, 0, dt * casadi.tan(delta) / self.wheelbase, 1],
+        ])
+        B = casadi.blockcat([
+            [0, 0],
+            [0, 0],
+            [dt, 0],  #
+            [0, dt * vel / (self.wheelbase * casadi.cos(delta) ** 2)],
+        ])
+        G = casadi.vertcat(vel * casadi.sin(psi) * psi * dt,
+                           -vel * casadi.cos(psi) * psi * dt,
+                           0,
+                           -(vel * delta) * dt / (self.wheelbase * casadi.cos(delta) ** 2))
+
+        # dynamics (discrete linear model)
+        f_expl = A @ z + B @ u + G  # discrete linear model
+
+        '''Constraints/bounds'''
+        if delta_bound is None:
+            delta_bound = self.delta_bound
+
+        if vel_bound is None:
+            vel_bound = self.vel_bound
+
+        if acc_bound is None:
+            acc_bound = self.acc_bound
+
+        if jerk_bound is None:
+            jerk_bound = self.jerk_bound
+
+        if delta_rate_bound is None:
+            delta_rate_bound = self.delta_rate_bound
+
+        # state bounds
+        # model.x_min = -np.inf
+        # model.x_max = np.inf
+        #
+        # model.y_min = -np.inf
+        # model.y_max = np.inf
+
+        model.v_min = vel_bound[0]
+        model.v_max = vel_bound[1]
+
+        # model.psi_min = -2 * np.pi
+        # model.psi_max = -2 * np.pi
+
+        # input bounds
+        model.delta_min = np.radians(delta_bound[0])  # minimum steering angle [rad]
+        model.delta_max = np.radians(delta_bound[1])  # maximum steering angle [rad]
+
+        model.acc_min = acc_bound[0]
+        model.acc_max = acc_bound[1]
+
+        # input rate bounds. Todo: setup as non-linear constraints
+        model.delta_rate_min = np.radians(delta_rate_bound[0])
+        model.delta_rate_max = np.radians(delta_rate_bound[1])
+
+        model.jerk_min = jerk_bound[0]
+        model.jerk_max = jerk_bound[1]
+
+        # (optional) Define initial conditions
+        if z0 is None:
+            if self.z0 is not None:
+                model.x0 = self.z0
+            else:
+                model.x0 = np.zeros(z.shape[0])
+        else:
+            model.x0 = z0
+
+
+        # Define model struct
+        params = types.SimpleNamespace()
+        params.wheelbase = wheelbase
+        # params.mass = 0.2
+        params.dt = dt
+
+        model.f_impl_expr = z_dot - f_expl
+        model.f_expl_expr = f_expl
+        model.x = z
         model.xdot = z_dot
         model.u = u
         model.u0 = u_prev

trajectory_following_ros2/coupled_kinematic_acados.py
@@ -144,10 +144,10 @@ class KinematicCoupledAcadosMPCNode(Node):
                                                                'set to "map", '
                                                                'otherwise use "odom".'))  # global frame: odom, map
         self.declare_parameter('ode_type', "continuous_kinematic_coupled")  # todo: also augmented, etc
-        self.declare_parameter('stage_cost_type', "LINEAR_LS")  # Acados specific (LINEAR_LS, NONLINEAR_LS, EXTERNAL)
-        self.declare_parameter('terminal_cost_type', "LINEAR_LS")  # Acados specific (LINEAR_LS, NONLINEAR_LS, EXTERNAL)
-        self.declare_parameter('control_rate', 50.0)
-        self.declare_parameter('debug_frequency', 4.0)
+        self.declare_parameter('stage_cost_type', "NONLINEAR_LS")  # Acados specific (LINEAR_LS, NONLINEAR_LS, EXTERNAL)
+        self.declare_parameter('terminal_cost_type', "NONLINEAR_LS")  # Acados specific (LINEAR_LS, NONLINEAR_LS, EXTERNAL)
+        self.declare_parameter('control_rate', 20.0)
+        self.declare_parameter('debug_frequency', 1.0)
         self.declare_parameter('distance_tolerance', 0.2)  # todo: move to goal checker node
         self.declare_parameter('speed_tolerance', 0.5)  # todo: move to goal checker node
         self.declare_parameter('wheelbase', 0.256)
@@ -156,6 +156,7 @@ class KinematicCoupledAcadosMPCNode(Node):
                                        description='The minimum lateral command (steering) to apply.'))  # in degrees
         self.declare_parameter('max_steer', 27.0)  # in degrees
 
+        np.set_printoptions(suppress=True)
         # MPC Parameters
         self.declare_parameter('max_steer_rate', 60 / 0.17)  # in degrees
         self.declare_parameter('max_speed', 1.5)  # in m/s
@@ -173,10 +174,10 @@ class KinematicCoupledAcadosMPCNode(Node):
                                1e-6)  # iteration finish param. todo: pass to mpc initializer solver options
 
         # tips for tuning weights (https://www.mathworks.com/help/mpc/ug/tuning-weights.html)
-        self.declare_parameter('R', [0.01, 0.01])
-        self.declare_parameter('Rd', [10., 100.])
-        self.declare_parameter('Q', [1.0, 1.0, 1.0, 0.01])
-        self.declare_parameter('Qf', [0.04, 0.04, 0.1, 0.01])
+        self.declare_parameter('R', [10., 100.]),  # [0.01, 0.01], [10., 100.]
+        self.declare_parameter('Rd', [10., 100.]),  #
+        self.declare_parameter('Q', [1.0, 1.0, 1.0, 100.0])
+        self.declare_parameter('Qf', [0.04, 0.04, 0.1, 10.0])
         self.declare_parameter('scale_cost', False)
 
         self.declare_parameter('path_topic', '/trajectory/path')  # todo: replace with custom message or action
@@ -842,7 +843,7 @@ class KinematicCoupledAcadosMPCNode(Node):
                 if status != 0:
                     # acados_solver.print_statistics()
                     # 0-success, 1-failure, 2-maximum number of iterations reached,
-                    # 3-minimum step sizze in QP solver reached, 4-qp solver failed
+                    # 3-minimum step size in QP solver reached, 4-qp solver failed
                     # todo: set a flag (verbosity) to enable/disable this
                     self.get_logger().info("acados returned status {} in"
                                            " closed loop iteration {}.".format(status, self.run_count))
@@ -876,14 +877,15 @@ class KinematicCoupledAcadosMPCNode(Node):
                 # self.get_logger().info(f'Cost: {cost}')
                 num_iter = self.controller.get_stats('sqp_iter')
                 stats = self.controller.get_stats('statistics')
-                solve_time_ = self.controller.get_stats('time_tot')
-                time_qp_solution_ = self.controller.get_stats('time_qp')
-                time_linearization_ = self.controller.get_stats('time_lin')
-                time_integrator_ = self.controller.get_stats('time_sim')
-                # acados_solver.print_statistics()
-                # self.get_logger().info(f"Elapsed time: {solver_dt}, total_time = {solve_time_}, num_iter = {num_iter},"
-                #       f"linearization_time = {time_linearization_}, integration_time = {time_integrator_}, "
-                #       f"qp time = {time_qp_solution_}  \n")
+                solve_time_ = self.controller.get_stats('time_tot') or -1
+                time_qp_solution_ = self.controller.get_stats('time_qp') or -1
+                time_linearization_ = self.controller.get_stats('time_lin') or -1
+                time_integrator_ = self.controller.get_stats('time_sim') or -1
+                # self.controller.print_statistics()
+
+                # self.get_logger().info(f"Elapsed time: {1 / solver_dt}, total_time = {1 / solve_time_}, num_iter = {num_iter},"
+                #       f"linearization_time = {1 / time_linearization_}, integration_time = {1 / time_integrator_}, "
+                #       f"qp time = {1 / time_qp_solution_}  \n")
 
                 warmstart_variables = {'z_ws': solution_dict['z_mpc'], 'u_ws': solution_dict['u_mpc'],
                                        'sl_ws': solution_dict['sl_mpc']}
@@ -900,11 +902,12 @@ class KinematicCoupledAcadosMPCNode(Node):
                 self.acc_cmd = u[0]
                 self.delta_cmd = u[1]
                 '''To get the velocity command: use either of the following:
-                1: Predicted velocity[k+1], i.e predicted_vel_state[1, 0]
+                1: Predicted velocity[k+1], i.e predicted_vel_state[1, 0]. [k + 1]
                 2: Current_velocity + integrate(acceleration), i.e current_speed + self.acc_cmd * self.sample_time
                 '''
                 self.velocity_cmd = predicted_vel_state[1, 0]  # float(predicted_vel_state[1])
                 # self.velocity_cmd = vel + self.acc_cmd * self.sample_time
+                self.get_logger().info(f"acc cmd: {self.acc_cmd}, delta cmd: {self.delta_cmd}, vel_cmd: {self.velocity_cmd}")
 
                 if self.saturate_input:
                     self.input_saturation()

trajectory_following_ros2/coupled_kinematic_do_mpc.py
@@ -162,7 +162,6 @@ class KinematicCoupledDoMPCNode(Node):
         self.declare_parameter('Rd', [10., 100.])
         self.declare_parameter('Q', [1.0, 1.0, 1.0, 0.01])
         self.declare_parameter('Qf', [0.04, 0.04, 0.1, 0.01])
-        self.declare_parameter('scale_cost', False)
 
         self.declare_parameter('path_topic', '/trajectory/path')  # todo: replace with custom message or action
         self.declare_parameter('speed_topic', '/trajectory/speed')  # todo: replace with custom message or action
@@ -177,7 +176,7 @@ class KinematicCoupledDoMPCNode(Node):
         self.declare_parameter('n_ind_search', 10)  # the number of points to check for when searching for the closest
         self.declare_parameter('smooth_yaw', False)
         self.declare_parameter('debug', False)  # displays solver output
-        self.declare_parameter('generate_mpc_model', True)  # generate and build model
+        self.declare_parameter('generate_mpc_model', False)  # generate and build model
         self.declare_parameter('build_with_cython', True)  # whether to use cython (recommended) or ctypes
         self.declare_parameter('model_directory',
                                '/f1tenth_ws/src/trajectory_following_ros2/data/model')  # don't use absolute paths
@@ -214,7 +213,6 @@ class KinematicCoupledDoMPCNode(Node):
             self.Qf = np.diag(self.get_parameter('Qf').value)
         else:
             self.Qf = self.Q
-        self.scale_cost = self.get_parameter('scale_cost').value
         self.horizon = int(self.get_parameter('horizon').value)
         self.prediction_time = self.get_parameter('prediction_time').value
 
@@ -236,6 +234,7 @@ class KinematicCoupledDoMPCNode(Node):
         self.n_ind_search = self.get_parameter('n_ind_search').value
         self.smooth_yaw = self.get_parameter('smooth_yaw').value
         self.debug = self.get_parameter('debug').value
+        self.generate_mpc_model = self.get_parameter('generate_mpc_model').value
 
         self.model_type = 'continuous' if 'continuous' in self.ode_type else 'discrete'
         
@@ -497,7 +496,8 @@ class KinematicCoupledDoMPCNode(Node):
                                                        acc_max=self.MAX_ACCEL,
                                                        max_iterations=self.max_iter,
                                                        tolerance=self.termination_condition,
-                                                       suppress_ipopt_output=True, model_type=self.model_type)
+                                                       suppress_ipopt_output=True, model_type=self.model_type,
+                                                       compile_model=self.generate_mpc_model)
         self.mpc_built = True
         self.get_logger().info("Finished setting up the MPC.")
 
@@ -767,6 +767,9 @@ class KinematicCoupledDoMPCNode(Node):
                 self.solver_iteration_count = self.Controller.mpc.solver_stats['iter_count']
                 self.solution_status = self.Controller.mpc.solver_stats['success']
                 # todo: publish solver stats
+
+                # if self.run_count % self.control_rate == 0:
+                #     self.get_logger().info(f"Accel command: {self.acc_cmd}, delta_cmd: {np.degrees(self.delta_cmd)}, run_count: {self.run_count}, Status: {self.solution_status}\n")
 
                 self.run_count += 1
 

trajectory_following_ros2/do_mpc/mpc_setup.py
@@ -17,7 +17,7 @@ class MPC(object):
                  Q=np.diag([1e-1, 1e-8, 1e-8, 1e-8]), R=np.diag([1e-3, 5e-3]),
                  Qf=np.diag([0.0, 0.0, 0.0, 0.0]), Rd=np.diag([0.0, 0.0]),
                  vel_bound=(-5.0, 5.0), delta_bound=(-23.0, 23.0), acc_bound=(-3.0, 3.0),
-                 max_iterations=20, tolerance=1e-6, suppress_ipopt_output=True):
+                 max_iterations=20, tolerance=1e-6, suppress_ipopt_output=True, compile_model=False):
         """Constructor for MPC"""
         self.model = vehicle.model
 
@@ -28,6 +28,7 @@ class MPC(object):
         self.Rd = Rd
 
         self.Ts = sample_time
+        self.compile_model = compile_model
         self.reference_states = np.zeros((self.horizon + 1, self.Q.shape[0]))
 
         self.mpc = self.initialize_mpc(model=self.model, horizon=self.horizon,
@@ -53,6 +54,10 @@ class MPC(object):
         self.mpc.set_tvp_fun(self.tvp_fun)
 
         self.mpc.setup()
+        if self.compile_model:
+            # self.mpc.compile_nlp(overwrite= False, cname='nlp.c', libname='nlp.so', compiler_command=None)
+            # todo: for now there is a bug in the do_mpc code. Compile using my method in casadi node and modify the solver self.mpc.S
+            pass
 
     def initialize_mpc(self, model, horizon, timestep=0.01, store_full_solution=False):
         """
@@ -198,7 +203,8 @@ def initialize_mpc_problem(reference_path, horizon=15, sample_time=0.02,
                            Q=None, R=None, Qf=None, Rd=None, wheelbase=0.256,
                            delta_min=-23.0, delta_max=23.0, vel_min=-10.0, vel_max=10.0,
                            ay_max=4.0, acc_min=-3.0, acc_max=3.0,
-                           max_iterations=100, tolerance=1e-6, suppress_ipopt_output=True, model_type='continuous'):
+                           max_iterations=100, tolerance=1e-6, suppress_ipopt_output=True, model_type='continuous',
+                           compile_model=False):
     """
     Get configured do-mpc modules:
     """
@@ -212,7 +218,8 @@ def initialize_mpc_problem(reference_path, horizon=15, sample_time=0.02,
 
     Controller = MPC(Vehicle, horizon=horizon, sample_time=sample_time, Q=Q, R=R, Qf=Qf, Rd=Rd, wheelbase=wheelbase,
                      vel_bound=(vel_min, vel_max), delta_bound=(delta_min, delta_max), acc_bound=(acc_min, acc_max),
-                     max_iterations=max_iterations, tolerance=tolerance, suppress_ipopt_output=suppress_ipopt_output)
+                     max_iterations=max_iterations, tolerance=tolerance, suppress_ipopt_output=suppress_ipopt_output,
+                     compile_model=compile_model)
 
     # Sim = Simulator(Vehicle, sample_time=sample_time)
     Sim = None